a6d493cd05d57a4e133a233c2cdd0e8d19d0a7fa
Cao-Cuong Do <88456061+cuongdc03@users.noreply.github.com>
2024-08-25 07:59:25 +0700

    fix authen feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,9 +26,7 @@ exports.login = async (req, res) => {
         const { email, password } = req.body;
 
         const user = await User.findOne({ where: { email } });
-  
-
-        if (!user || await !bcrypt.compare(password, user.password)) {
+        if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(401).json({ message: 'Thông tin đăng nhập không hợp lệ' });
         }
 
@@ -36,7 +34,6 @@ exports.login = async (req, res) => {
 
         res.status(200).json({ message: 'Đăng nhập thành công', token });
     } catch (error) {
-        console.log(error)
         res.status(500).json({ message: 'Lỗi khi đăng nhập', error });
     }
 };
